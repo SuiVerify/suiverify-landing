acce5f7b6c0078b3b96e15a3fa491bc8e6e263de
Ashwin-3cS <flashashwin2004@gmail.com>
2025-10-03 02:13:52 +0530

    fix: hero card edited

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -142,8 +142,8 @@ const HeroSection: React.FC = () => {
           >
             {[
               {
-                title: "Secure Verification",
-                description: "Offchain Verifiable Computation of DID's",
+                title: "Nautilus Verification",
+                description: "Offchain Verifiable Computation of DID's in Nautilus",
                 icon: (
                   <svg className="w-8 h-8" style={{ color: colors.primary }} fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
