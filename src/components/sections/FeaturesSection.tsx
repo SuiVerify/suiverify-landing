21bd2f2294a0cabd62ecb735d968edfbbf797ce7
Ashwin-3cS <flashashwin2004@gmail.com>
2025-10-02 21:02:44 +0530

    fix: logo added for features section

--- src/components/sections/FeaturesSection.tsx
+++ src/components/sections/FeaturesSection.tsx
@@ -10,9 +10,7 @@ const FeaturesSection: React.FC = () => {
       title: "Nautilus",
       description: "Nautilus for offchain computation and verifiability onchain - enabling secure identity verification processing",
       icon: (
-        <svg className="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20">
-          <path fillRule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-        </svg>
+        <img src="/nautilus.png" alt="Nautilus" className="w-8 h-8 object-contain" />
       ),
       gradient: colors.gradients.primary
     },
@@ -20,10 +18,7 @@ const FeaturesSection: React.FC = () => {
       title: "Seal",
       description: "Seal for encrypting user KYC documents and can be then decrypted only by Government addresses using SEAL protocol",
       icon: (
-        <svg className="w-8 h-8" style={{ color: colors.darkerNavy }} fill="currentColor" viewBox="0 0 20 20">
-          <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
-          <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.51-1.31c-.562-.649-1.413-1.076-2.353-1.253V5z" clipRule="evenodd" />
-        </svg>
+        <img src="/Seal_logo.png" alt="Seal" className="w-8 h-8 object-contain rounded-full" />
       ),
       backgroundColor: colors.lightBlue
     },
@@ -31,9 +26,7 @@ const FeaturesSection: React.FC = () => {
       title: "Walrus",
       description: "Walrus for storage of the documents to have our programmability of the data intact with decentralized storage",
       icon: (
-        <svg className="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20">
-          <path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
-        </svg>
+        <img src="/walrus.svg" alt="Walrus" className="w-8 h-8 object-contain" />
       ),
       gradient: colors.gradients.primary
     }
@@ -135,7 +128,7 @@ const FeaturesSection: React.FC = () => {
                       <img src="/Seal_logo.png" alt="Seal" className="w-8 h-8 object-contain rounded-full" />
                     </div>
                     <h4 className="font-bold text-sm mb-1" style={{ color: colors.white }}>Seal</h4>
-                    <p className="text-xs" style={{ color: colors.lightBlue }}>Document Encryption</p>
+                    <p className="text-xs" style={{ color: colors.lightBlue }}>Document Encryption & Decryption</p>
                   </div>
                 </div>
               </div>
@@ -148,7 +141,7 @@ const FeaturesSection: React.FC = () => {
                       <img src="/nautilus.png" alt="Nautilus" className="w-8 h-8 object-contain" />
                     </div>
                     <h4 className="font-bold text-sm mb-1" style={{ color: colors.white }}>Nautilus</h4>
-                    <p className="text-xs" style={{ color: colors.lightBlue }}>Off-chain Compute</p>
+                    <p className="text-xs" style={{ color: colors.lightBlue }}>Off-chain Computation</p>
                   </div>
                 </div>
               </div>
